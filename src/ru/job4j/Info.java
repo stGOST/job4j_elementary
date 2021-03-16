fc27282bf62b5c69bcb8e1a9014918c341793cbe
stGOST <dda.90@mail.ru>
2021-03-16 22:43:09 +0500

    3.4. Заголовок в файле java [#498001]

---
+++ src/ru/job4j/Info.java
@@ -0,0 +1,10 @@
+package ru.job4j;
+
+import java.util.Date;
+
+public class Info {
+    public static void main(String[] args) {
+        System.out.println(new Date().toString());
+        System.out.println("16.03.2021");
+    }
+}
