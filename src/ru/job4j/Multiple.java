46693f5bac74977d6d07a95386ee9fbbd76be12e
stGOST <dda.90@mail.ru>
2021-03-16 22:33:54 +0500

    3.3. Разбор первой программы. [#498000]

---
+++ src/ru/job4j/Multiple.java
@@ -0,0 +1,10 @@
+package ru.job4j;
+
+public class Multiple {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 9; i++)
+        {
+            System.out.println("1*"+i+"="+i);
+        }
+    }
+}
